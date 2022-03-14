57c0478bf5a7b8f885c7da92a3edb05cd4744a26
Oladele Marwan <oladelemarwan@gmail.com>
2022-03-14 17:23:52 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,18 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id);
+    setTours(newTour);
+  };
+
   const fetchTours = async () => {
     setLoading(true);
     try {
       const response = await fetch(url);
       const tours = await response.json();
       setLoading(false);
-      setTours(tours)
+      setTours(tours);
     } catch (error) {
       setLoading(false);
       console.log(error);
@@ -33,9 +38,21 @@ function App() {
       </main>
     );
   }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>Refresh</button>
+        </div>
+      </main>
+    );
+  }
+
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
